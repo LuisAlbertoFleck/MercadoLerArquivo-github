df69cd848445a5df2e720f02ca09d0bd3827cb63
LuisAlbertoFleck <luisfleck@gmail.com>
2021-05-09 16:16:41 -0300

    leitura e possivel alteracao de arquivo

---
+++ Mercado/src/entidades/Vestuario.java
@@ -0,0 +1,23 @@
+package entidades;
+
+public class Vestuario extends Produto {
+	private int quantidadeEstoque;
+
+	public Vestuario(double preco, String descricao, String tipo, int quantidadeEstoque) {
+		super(preco, descricao, tipo);
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public int getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	} 
+
+	public void setQuantidadeEstoque(int quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	@Override
+	public String toString() {
+		return "Vestuario [quantidadeEstoque=" + quantidadeEstoque + ", toString()=" + super.toString() + "]";
+	}
+}
